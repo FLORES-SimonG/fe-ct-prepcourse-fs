4fab6d54f3c8fe56088687b43cf4a0829c595aad
Simon G. Flores <simonflores_10@hotmail.com>
2023-11-07 17:25:01 +0100

    complete el modulo05

---
+++ apuntes_javascript/arrays.js
@@ -0,0 +1,24 @@
+//METODOS:
+
+// array.push()
+// array.pop()
+// array.unshift()
+// array.shift()
+// string.split('') --> me lo transforma en una array separada
+// string.join('')--> transformo en una array en una string
+
+var numeros =[1,2,3,4];
+numeros.forEach((num)=>console.log(num));
+numeros.forEach((num)=>{
+    if(num===3){
+        console.log(num);
+    }
+})
+
+var masUno=numeros.map((map)=>{
+    return num+1;
+});
+// rta--> [2,3,4,5]
+
+
+// mas info en : https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array
